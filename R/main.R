ed4ab9ec5849c20d5d99a6437bc08e025b6c9982
Valentin <lvtkiet.work@gmail.com | kiet.le0303@hcmut.edu.vn>
2022-03-08 09:11:33 +0700

    cập nhật i2, đã hiển thị được 10 đất nước đầu tiên.

--- R/main.R
+++ R/main.R
@@ -16,7 +16,6 @@ bigTable <- (read.csv("owid-covid-data.csv", header = TRUE)) #bigTable là bản
 
 # truy cập vào từng phần tử của mảng bằng $
 # công việc: sau khi đọc R phải liệt ra các nước Canada,  Greenland và United States thôi
-
 table <- subset(bigTable, location == "Canada" |location == "Greenland" | 
     location == "United States") #tách Canada, loction và Greenland ra theo đề bài
 
@@ -29,9 +28,24 @@ numLoca <- 1
 for ( i in seq ( 1, (length(locations) - 1), by = 1 ) ) {
     if (locations[i] != locations [i + 1]) {
         numLoca <- numLoca + 1
-
     }
 }
 
+#hiển thị tên 10 đất nước đầu tiên
+iso_code <- c("AFG")
+country <- c("Afghanistan")
+indexCountry <- 1
+for ( i in seq ( 1, (length(locations) - 1), by = 1 ) ) {
+    if (locations[i] != locations [i + 1]) {
+        indexCountry <- indexCountry + 1
 
+        #sử dụng vector <- append (vector, element) để thêm cái phần tử đó vào sau đít vector
+        iso_code <- append (iso_code, bigTable $ iso_code [i + 1] )
+        country <- append (country, bigTable $ country [i + 1] )
+    }
+    if (indexCountry >= 10) { break }
+}
+
+#i2) Bảng hiển thị 10 đất nước đầu tiên
+top10Countries <- data.frame (iso_code, country)
 
