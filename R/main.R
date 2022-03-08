7045187868bcff255cc24a66966df30f27f81a10
Valentin <lvtkiet.work@gmail.com | kiet.le0303@hcmut.edu.vn>
2022-03-08 16:17:55 +0700

    Một phần i4. Tuy nhiên vẫn chưa hiểu vì sao i4 bước lập bảng tính tổng new_cases của từng châu lục lại sai? Cần sửa

--- R/main.R
+++ R/main.R
@@ -1,58 +1,72 @@
 library(readr)
 library(ggplot2)
 
-#Tính các ký tự 1 2 3 4 từ thực tế
+# Tính các ký tự 1 2 3 4 từ thực tế
 kytu1 <- 4
 kytu2 <- 2
 kytu3 <- 6
 kytu4 <- 3
 kq <- (kytu1 + kytu2 + kytu3 + kytu4) %% 6
-stt <- c (11,12,13)
+stt <- c(11, 12, 13)
 
 
-bigTable <- (read.csv("owid-covid-data.csv", header = TRUE)) #bigTable là bảng thô chưa được chỉnh sửa
+bigTable <- (read.csv("owid-covid-data.csv", header = TRUE)) # bigTable là bảng thô chưa được chỉnh sửa
 
 # truy cập vào từng phần tử của mảng bằng $
 # công việc: sau khi đọc R phải liệt ra các nước Canada,  Greenland và United States thôi
-table <- subset(bigTable, location == "Canada" |location == "Greenland" | 
-    location == "United States") #tách Canada, loction và Greenland ra theo đề bài
+table <- subset(bigTable, location == "Canada" | location == "Greenland" |
+    location == "United States") # tách Canada, loction và Greenland ra theo đề bài
 
-allDate <- bigTable $ date  #tìm ngày tháng
-date <- table $ date
+allDate <- bigTable$ date # tìm ngày tháng
+date <- table$ date
 
-#i1 
-#tính số lượng đất nước
-numLoca <- length ( unique (bigTable $ location) )
+# i1
+# tính số lượng đất nước
+numLoca <- length(unique(bigTable$ location))
 
-#hiển thị tên 10 đất nước đầu tiên
+# hiển thị tên 10 đất nước đầu tiên
 iso_code <- c("AFG")
 country <- c("Afghanistan")
-locations <- bigTable $ location
+locations <- bigTable$ location
 indexCountry <- 1
-for ( i in seq ( 1, (length(locations) - 1), by = 1 ) ) {
-    if (locations[i] != locations [i + 1]) {
+for (i in seq(1, (length(locations) - 1), by = 1)) {
+    if (locations[i] != locations[i + 1]) {
         indexCountry <- indexCountry + 1
 
-        #sử dụng vector <- append (vector, element) để thêm cái phần tử đó vào sau đít vector
-        iso_code <- append (iso_code, bigTable $ iso_code [i + 1] )
-        country <- append (country, bigTable $ country [i + 1] )
+        # sử dụng vector <- append (vector, element) để thêm cái phần tử đó vào sau đít vector
+        iso_code <- append(iso_code, bigTable$ iso_code[i + 1])
+        country <- append(country, bigTable$ country[i + 1])
     }
-    if (indexCountry >= 10) { break }
+    if (indexCountry >= 10) {
+        break
+    }
 }
 
-#i2 
-#Bảng hiển thị 10 đất nước đầu tiên
-top10Countries <- data.frame (iso_code, country)
+# i2
+# Bảng hiển thị 10 đất nước đầu tiên
+top10Countries <- data.frame(iso_code, country)
 
-#i3 
-#tìm số lượng châu lục trong tập mẫu
-numConti <- length ( unique (bigTable $ continent) )
-#OWID_AFR không có tên châu lục có cần tính không?
+# i3
+# tìm số lượng châu lục trong tập mẫu
+numConti <- length(unique(bigTable$ continent))
+# OWID_AFR không có tên châu lục có cần tính không?
 
 
-#i45
-#tính số lượng dữ liệu thu thập được trong từng châu lục và tổng số
-#ở đây, tôi giả sử "dữ liệu" đề bài nói đến chính là new_cases và new_deaths
+# i45
+# tính số lượng dữ liệu thu thập được trong từng châu lục và tổng số
+# ở đây, tôi giả sử "dữ liệu" đề bài nói đến chính là new_cases và new_deaths
 
-#Đầu tiên, lập bảng newcases của từng châu lục
+# Đầu tiên, lập bảng newcases của từng châu lục (continents)
+continents <- unique(bigTable$ continent)
 
+new_cases <- c()
+for (string in continents) {
+    new_cases <- append(new_cases, sum(
+        (subset(bigTable, continent == string)) $ new_cases
+    ))
+}
+#hàm subset dùng để tách một bảng con từ bảng cho trước với điều kiện nào đó
+#hàm append để chèn thêm 1 phần tử vào cuối một lục
+#tại sao bước tính new_cases lại sai?
+
+test <- data.frame (continents, new_cases)
