0ffd8145c6091225bf9fe3278f55c90b76dfbf99
Valentin <lvtkiet.work@gmail.com | kiet.le0303@hcmut.edu.vn>
2022-03-10 09:47:27 +0700

    Thêm một số chỉ dẫn hàm vào đầu bài Sửa lại cách tìm min max death case

--- R/mission2.R
+++ R/mission2.R
@@ -1,11 +1,17 @@
+#một số hàm được sử dụng
+#tính tứ phân vị, dùng hàm quantile() hoặc summary()
+#na.omit() để loại bỏ giá trị NA
+
+
 load ("workspace/preTask.RData")
 load ("workspace/mission1.RData")
 
 #số tử nhiều nhất
-maxDeath <- max ( (subset(bigTable, new_deaths >= 0)) $ new_deaths)
-minDeath <- min ( (subset(bigTable, new_deaths <= maxDeath)) $ new_deaths) 
+maxDeath <- max (na.omit(bigTable $ new_deaths) )
+minDeath <- min (na.omit(bigTable $ new_deaths) )
 #trường hợp này ra số âm nhưng không biết xử lý thế nào
 
-maxCase <- max ( (subset(bigTable, new_cases >= 0)) $ new_cases)
-minCase <- min ( (subset(bigTable, new_cases <= maxCase)) $ new_cases) 
+maxCase <- max (na.omit(bigTable $ new_cases) )
+minCase <- min (na.omit(bigTable $ new_cases) )
 
+#tính tứ phân vị, dùng hàm quantile() hoặc summary()
