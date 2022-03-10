23dbb958e3fb74224a141d32f08801efed335db6
Valentin <lvtkiet.work@gmail.com | kiet.le0303@hcmut.edu.vn>
2022-03-10 11:15:27 +0700

    Dù không hiểu đề lắm nhưng làm nốt ii5

--- R/mission2.R
+++ R/mission2.R
@@ -1,26 +1,44 @@
-#một số hàm được sử dụng
-#tính tứ phân vị, dùng hàm quantile() hoặc summary()
-#na.omit() để loại bỏ giá trị NA
-#hàm summary() cho cả giá trị trung bình
-#sd() để tính giá trị lệch chuẩn
+# một số hàm được sử dụng
+# tính tứ phân vị, dùng hàm quantile() hoặc summary()
+# na.omit() để loại bỏ giá trị NA
+# hàm summary() cho cả giá trị trung bình
+# sd() để tính giá trị lệch chuẩn
 
 
-load ("workspace/preTask.RData")
-load ("workspace/mission1.RData")
+load("workspace/preTask.RData")
+load("workspace/mission1.RData")
 
-#số tử nhiều nhất
-maxDeath <- max (na.omit(bigTable $ new_deaths) )
-minDeath <- min (na.omit(bigTable $ new_deaths) )
-#trường hợp này ra số âm nhưng không biết xử lý thế nào
+rmNaDeathList <- na.omit(bigTable$ new_deaths)
+rmNaCaseList <- na.omit(bigTable$ new_cases)
 
-maxCase <- max (na.omit(bigTable $ new_cases) )
-minCase <- min (na.omit(bigTable $ new_cases) )
 
-#tính tứ phân vị và giá trị trung bình, giá trị lệch chuẩn
-quartileDeath <- summary (na.omit (bigTable $ new_deaths)) [c (2,3,5)]
-meanDeath <-summary (na.omit (bigTable $ new_deaths)) [4]
-stdDeath <- sd (bigTable$new_deaths, na.rm = TRUE)
+# số tử nhiều nhất
+maxDeath <- max(rmNaDeathList)
+minDeath <- min(rmNaDeathList)
+# trường hợp này ra số âm nhưng không biết xử lý thế nào
 
-quartileCase <- summary (na.omit (bigTable $ new_cases)) [c (2,3,5)]
-meanCase <- summary (na.omit (bigTable $ new_cases)) [4]
-stdCase <- sd (bigTable $ new_cases, na.rm = TRUE)
+maxCase <- max(rmNaCaseList)
+minCase <- min(rmNaCaseList)
+
+# tính tứ phân vị và giá trị trung bình, giá trị lệch chuẩn
+quartileDeath <- summary(rmNaDeathList)[c(2, 3, 5)]
+meanDeath <- summary(rmNaDeathList)[4]
+stdDeath <- sd(bigTable$new_deaths, na.rm = TRUE)
+
+quartileCase <- summary(rmNaCaseList)[c(2, 3, 5)]
+meanCase <- summary(rmNaCaseList)[4]
+stdCase <- sd(bigTable$ new_cases, na.rm = TRUE)
+
+
+# i5
+# câu này không hiểu rõ đề
+# đếm xem bao nhiêu outliers của Death
+deathIQR <- quartileDeath[3] - quartileDeath[1]
+deathOutliers <- length(rmNaDeathList[rmNaDeathList < quartileDeath[1] - 1.5 * deathIQR |
+    rmNaDeathList > quartileDeath[3] + 1.5 * deathIQR])
+
+# đếm xem bao nhiêu outliers của Case
+caseIQR <- quartileCase[3] - quartileCase[1]
+caseOutliers <- length(rmNaCaseList[rmNaCaseList < quartileCase[1] - 1.5 * caseIQR |
+    rmNaCaseList > quartileCase[3] + 1.5 * caseIQR])
+
